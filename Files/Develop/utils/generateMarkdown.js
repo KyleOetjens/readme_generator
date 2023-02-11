fff28921295090cd8d4fdce2e7ca9d59e88d6aea
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-02-11 16:02:52 -0600

    commit

--- Files/Develop/utils/generateMarkdown.js
+++ Files/Develop/utils/generateMarkdown.js
@@ -4,17 +4,102 @@ const fs = require('fs');
 */
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(data) { 
+function renderLicenseBadge(data) {
     let licenseBadge = data.projectLicense
     switch (licenseBadge) {
         case `Apache License 2.0`: {
             let licenseBadge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
-            generateMarkdown(data,licenseBadge)
+            let licenseLink = `https://opensource.org/licenses/Apache-2.0`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
         }
-            break;}
-    console.log(`made it`);
-}
+            break;
+        case `GNU General Public License v3.0`: {
+            let licenseBadge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+            let licenseLink = `https://www.gnu.org/licenses/gpl-3.0`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `MIT License`: {
+            let licenseBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+            let licenseLink = `https://opensource.org/licenses/MIT`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `BSD 2-Clause "Simplified" License`: {
+            let licenseBadge = `![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)`
+            let licenseLink = `https://opensource.org/licenses/BSD-2-Clause`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `BSD 3-Clause "New" or "Revised" License`: {
+            let licenseBadge = `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+            let licenseLink = `https://opensource.org/licenses/BSD-3-Clause`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `Boost Software License 1.0`: {
+            let licenseBadge = `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+            let licenseLink = `https://www.boost.org/LICENSE_1_0.txt`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `Creative Commons Zero v1.0 Universal`: {
+            let licenseBadge = `![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)`
+            let licenseLink = `http://creativecommons.org/publicdomain/zero/1.0/`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `Eclipse Public License 2.0`: {
+            let licenseBadge = `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`
+            let licenseLink = `https://opensource.org/licenses/EPL-1.0`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `GNU Affero General Public License v3.0`: {
+            let licenseBadge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+            let licenseLink = `https://www.gnu.org/licenses/gpl-3.0`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `GNU General Public License v2.0`: {
+            let licenseBadge = `![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)`
+            let licenseLink = `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `Mozilla Public License 2.0`: {
+            let licenseBadge = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+            let licenseLink = `https://opensource.org/licenses/MPL-2.0`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+        case `The Unlicense`: {
+            let licenseBadge = `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`
+            let licenseLink = `http://unlicense.org/`
+            let licenseMd = '## License'
+            generateMarkdown(data, licenseBadge,licenseMd)
+        }
+            break;
+            case `None`: {
+                let licenseBadge = ``
+                let licenseLink = ``
+                let licenseMd = ''
+                generateMarkdown(data, licenseBadge,licenseMd)
+            }
+}}
 
+/*
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) { }
@@ -22,9 +107,10 @@ function renderLicenseLink(license) { }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) { }
+*/
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data,licenseBadge) {
+function generateMarkdown(data, licenseBadge,licenseMd) {
     console.log(data.projectTitle);
     const mdFile = `# ${data.projectTitle}
 
@@ -54,7 +140,7 @@ ${data.projectUsage}
 ## Credits
 
 
-## License
+${licenseMd}
 
 ${data.projectLicense}
 ---
@@ -82,11 +168,11 @@ ${data.projectTest}`;
     createReadme(mdFile)
 }
 function createReadme(mdFile) {
-    fs.writeFile(`README.MD`, mdFile, (err) =>
+    fs.writeFileSync(`README.MD`, mdFile, (err) =>
         err ? console.error(err) : console.log(`Check out your README`)
     )
 }
 module.exports = {
     renderLicenseBadge,
     generateMarkdown
-  };
+};
